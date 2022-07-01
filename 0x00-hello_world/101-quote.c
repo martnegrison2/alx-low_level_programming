eddd7163987e197188e661610b709c7592d55373
martnegrison2 <martin.nnamdi@yahoo.com>
2022-06-30 21:58:56 -0700

    low_language

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 
-#include <string.h>
-
-#include <unistd.h>
 
 /**
- *
- *  *  * main -Entry point
- *
- *   *   * Description: 'A new project abot using puts with special symbols'
- *
- *    *    * Return: Always 1 (success)
- *
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 1 (Success)
  */
 
 int main(void)
-
 {
 
-
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-
+	fprintf(stderr, "and that piece of art is useful\" - \
+			Dora Korpar, 2015-10-19\n");
 
 
 	return (1);
-
 }
