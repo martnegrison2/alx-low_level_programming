2cf317c5a87b2fc885a93370a2d297710f5ba170
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-03 13:33:06 -0700

    betty

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,8 @@
  *     * Return: should be always 0.
  *
  */
-int main(void)
+	int
+main(void)
 {
 	int digit1, digit2;
 	for (digit1 = 0; digit1 < 9; digit1++)
@@ -18,7 +19,7 @@ int main(void)
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0')
+			putchar((digit2 % 10) + '0');
 				if (digit1 == 8 && digit2 == 9)
 					continue;
 			putchar(',');
