e437850c78e537e74b4c8d9be1e7ca8dc64561be
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-05 20:48:03 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  *  *print_alphabet - Alphabet in lowercase
+ *
+ *   *   *
+ *
+ *    *    *Return:0
+ *
+ *     *     *
+ *
+ */
+
+void print_alphabet(void)
+{
+	char d;
+	
+	for (d = 'a'; r <= 'z'; d++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
