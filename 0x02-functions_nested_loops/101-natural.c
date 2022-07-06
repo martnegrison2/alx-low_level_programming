c60a434ed0caf8dd0ca7a9ca84f0bf2f39b55c18
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-06 08:18:01 -0700

    putchar

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- *  *  * main - prints all possible different combinations of three digits
+ *  *  * main - prints all possible different
+ *  combinations of three digits
  *   *   * Return: Always 0 (Success)
  */
 
