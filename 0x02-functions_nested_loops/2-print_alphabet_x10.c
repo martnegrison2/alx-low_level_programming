f1898c259fbd3f056bc3014b4ade4794f0eb505d
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-05 21:00:00 -0700

    putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  *  *print_alphabet_x10 - 10 times Alphabet in lowercase
+ *
+ *   *   *
+ *
+ *    *    *Return:0
+ *
+ *     *     *
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char d;
+
+	int a = 0;
+	while (a < 10)
+	{
+		for (r = 'a'; r <= 'z'; r++)
+		{
+			_putchar(r);
+		}
+		_putchar('\n');
+		a++;
+	}
+}
