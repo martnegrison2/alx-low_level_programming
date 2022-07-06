bceb9d0528a9083647a1af2f94b0b9ae176dbb49
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-06 13:26:00 -0700

    Putchar

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
 
 /**
- *  *  * main - prints all possible different
- *  combinations of three digits
- *   *   * Return: Always 0 (Success)
+ *  *  main - list all natural numbers multiples of 3 & 5
+ *   *   Return: Always 0.
  */
 int main(void)
 {
-	int n, m, l;
+	int num, sum;
 
-	for (n = 48; n < 58; n++)
+	sum = 0;
+	for (num = 0; num < 1024; num++)
 	{
-		for (m = 49; m < 58; m++)
+		if (num % 3 == 0 || num % 5 == 0)
 		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
+			sum += num;
 		}
 	}
-	putchar('\n');
+	printf("%d\n", sum);
 	return (0);
 }
