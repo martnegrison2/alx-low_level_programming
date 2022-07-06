44c6e9ed081549c61d2f9594096de82c644eea00
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-05 21:16:40 -0700

    putchar

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,11 @@
 
 /**
  *    *_islower - check lowercase character.
- *
  *      *
- *
  *        *@c: integer 1 or 0
- *
  *          *
- *
  *            *Return: 1 lowercase 0 Not
- *
  *              *
- *
  */
 
 int _islower(int c)
