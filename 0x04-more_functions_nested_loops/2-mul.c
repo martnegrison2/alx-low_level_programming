54e8248a4b9aae363292ffb3465da8410128955f
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-08 14:00:45 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+
+/**
+ *  * mul - multiple of two integer
+ *    * @a: as int
+ *     * @b: as int
+ *       * Return: Always 0
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b)
+}
+
