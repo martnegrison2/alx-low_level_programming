4d1177a9fcc2d2ca62e7dd3888848f1938c23974
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-08 13:38:07 -0700

    nested

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  *  * _isupper - check for uppercase
- *    *    * @c: character
- *      *      * Return: 1 if it is uppercase
+ *  * _isupper - check for uppercase
+ *    * @c: character
+ *      * Return: 1 if it is uppercase
  */
 
 int _isupper(int c)
