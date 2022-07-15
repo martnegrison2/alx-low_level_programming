8d131bda2ae13dab423e0fa3137b882ea73631a4
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-14 17:37:19 -0700

    more array

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,7 +12,6 @@ char *rot13(char *s)
 	int i, j;
 
 	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	for (i = 0; *(s + i); i++)
 	{
