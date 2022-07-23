b7e66739f279cf8f2b4aa3e1b365df499194832f
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-23 15:03:46 -0700

    will succeed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,47 +1,42 @@
-
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - coins to make change for an amount of money
+ * main - prints the minimum number of coins
  *
- * @argc: argument counter
+ * @argc: count
  *
- * @argv: array of pointer to strings
+ * @argv: array
  *
- * Return: 0 if no errors, else
+ * Return: 0
  */
 
-int main(int argc, char *argv[])	
-
+int main(int argc, char *argv[])
 {
-	int a, n = 0, i, t;
-	int c[5] = {25, 10, 5, 2, 1};
+	int cents, ncoins = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
-		puts("Error");
+		printf("Error\n");
 		return (1);
 	}
-	a = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
-	if (a <= 0)
+	while (cents > 0)
 	{
-		puts("0");
-		return (1);
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1
 	}
-	else
-	{
-		for (i = 0; i < 5; i++)
-		{
-			t = a / c[i];
-			a -= t * c[i];
-			n += t;
-
-			if (a == 0)
-				break;
-		}
-	}
-	printf("%d\n", n);
+	printf("%d\n", ncoins);
 	return (0);
 }
 
