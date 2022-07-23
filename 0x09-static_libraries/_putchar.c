1e408397796a52d08d28f05e22736d6c53cae1d5
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-23 15:28:22 -0700

    putchar

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes
+ *
+ * @c: the character to print
+ *
+ * Return: 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
