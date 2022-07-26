d1abc9249bceb1da4277fdec94d9275c5e76879a
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-26 14:44:04 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 	len2 = i;
 	s2 = tmp2;
 	str_copy = malloc((len1 + len2) *sizeof(char) + 1);
-	if (str_copy == NUL)
+	if (str_copy == NULL)
 			return (NULL);
 	j = 0;
 	while (j < len1)
