bf70f13dc176df3b556283f9cb5080d12cdaf119
martnegrison2 <martin.nnamdi@yahoo.com>
2022-07-28 15:58:18 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,7 +13,6 @@
 int main(int argc, char *argv[])
 {
 	unsigned long mul;
-
 	int i, j;
 
 	if (argc != 3)
@@ -30,7 +29,6 @@ int main(int argc, char *argv[])
 	}
 	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
-
 	return (0);
 }
 
