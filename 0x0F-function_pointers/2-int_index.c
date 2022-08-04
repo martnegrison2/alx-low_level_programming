a947351a1dad719a2e9c9c99f9c23ee9ae355007
martnegrison2 <martin.nnamdi@yahoo.com>
2022-08-04 01:50:42 -0700

    aspire

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,6 @@
  */
 
 int is_98(int elem)
-
 {
 	return (98 == elem);
 }
@@ -24,7 +23,6 @@ int is_98(int elem)
  */
 
 int is_strictly_positive(int elem)
-
 {
 	return (elem > 0);
 }
@@ -38,7 +36,6 @@ int is_strictly_positive(int elem)
  */
 
 int abs_is_98(int elem)
-
 {
 	return (elem == 98 || -elem == 98);
 }
@@ -50,7 +47,6 @@ int abs_is_98(int elem)
  */
 
 int main(void)
-
 {
 	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
 	int index;
