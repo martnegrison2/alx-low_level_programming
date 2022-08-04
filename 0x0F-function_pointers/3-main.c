a46c41ebfd24444a4fe8632f7ce64bfbaa31eaaa
martnegrison2 <martin.nnamdi@yahoo.com>
2022-08-04 16:00:54 -0700

    aspire

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,41 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- *main - program that perfroms simple operations
+ * main - main function
  *
- *@argc: number of arguments
+ * @argc: argument count
  *
- *@argv: array of arguments
+ * @argv: string of arguments in array
  *
- *Return: Always 0 (Success)
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int a, b;
+	int (*o)(int, int);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-	func = get_op_func(argv[2]);
-	if (!func)
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
-		o = *argv[2];
-		if ((o == '/' || o == '%') && arg2 == 0)
-		{
-			printf("Error\n");
-			exit(100);
-		}
-		result = func(arg1, arg2);
-		printf("%d\n", result);
-		return (0);
 	}
+	o = get_op_func(argv[2]);
+	if (o == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%d\n", o(a, b));
+	return (0);
+}
 
