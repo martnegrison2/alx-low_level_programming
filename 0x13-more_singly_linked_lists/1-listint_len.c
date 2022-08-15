d81964b6a7c89c4353bd6cadaf85e313379cc1d7
martnegrison2 <martin.nnamdi@yahoo.com>
2022-08-15 16:08:25 -0700

    more singly

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len -returns the number of elements in a linked list.
+ *
+ * @h: the first node of the list
+ *
+ * Return: always 0
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
+
